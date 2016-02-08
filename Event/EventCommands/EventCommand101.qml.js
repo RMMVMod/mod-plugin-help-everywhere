b682c70810b01ed29421f765b6e8876c7bd5842e
Yangff <Yangff@users.noreply.github.com>
2016-02-08 23:10:14 +0800

    Update EventCommand101.qml.js
    new apis

--- Event/EventCommands/EventCommand101.qml.js
+++ Event/EventCommands/EventCommand101.qml.js
@@ -2,18 +2,12 @@ var ModAPI = require('modapi')
 var _ = require('lodash')
 var qml = ModAPI.QMLFile("Event/EventCommands/EventCommand101.qml")
 
-var previewButton = qml.getObjectById("button")
-previewButton.node.object("anchors.right", null)
-previewButton.node.object("anchors.bottom", null)
-
-var container = previewButton.parent.newObject().createNode()
-container.describe = "ControlsRow"
-container.object("anchors.right", "parent.right")
-container.object("anchors.bottom", "parent.bottom")
-
-var pheButton = container.newObject()
-pheButton.createNode().describe = "PluginHelpEverywhereButton"
-
-previewButton.parent = container
+var previewButton = qml.root.getObjectById("button")
+previewButton.remove("anchors.right")
+previewButton.remove("anchors.bottom")
+var x = previewButton.parent
+var container = ModAPI.QMLCompile("ControlsRow {anchors.right: parent.right; anchors.bottom: parent.bottom; PluginHelpEverywhereButton{} } ")
+x.add(container)
+container.add(previewButton)
 
 qml.save()
