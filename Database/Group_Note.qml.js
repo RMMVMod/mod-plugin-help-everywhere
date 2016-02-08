ff8584a2f5203a8cb4d59cf995f2a5ec6c734991
Yangff <Yangff@users.noreply.github.com>
2016-02-08 23:10:36 +0800

    Update Group_Note.qml.js
    new apis

--- Database/Group_Note.qml.js
+++ Database/Group_Note.qml.js
@@ -2,18 +2,10 @@ var ModAPI = require('modapi')
 var _ = require('lodash')
 var qml = ModAPI.QMLFile("Database/Group_Note.qml")
 
-var textArea = qml.getObjectById("item")
-
-var container = textArea.parent.newObject().createNode()
-container.describe = "Item"
-container.object("width", "item.itemWidth")
-container.object("height", "item.itemHeight")
-
-textArea.parent = container
-
-var pheButton = container.newObject().createNode()
-pheButton.describe = "PluginHelpEverywhereButton"
-pheButton.object("anchors.bottom", "item.top")
-pheButton.object("anchors.right", "item.right")
+var textArea = qml.root.getObjectById("item")
+var container = ModAPI.QMLCompile("Item { width: item.itemWidth; height: item.itemHeight; PluginHelpEverywhereButton {anchors.bottom:item.top; anchors.right:item.right }}")
+var x = textArea.parent
+container.add(textArea)
+x.add(container)
 
 qml.save()
